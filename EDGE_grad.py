Correct the sign of loss and the calculation of accuracy

EDGE_grad.py
@@ -434,7 +434,7 @@ def EDGE_top(X,Y,U=20):
 
 	# Repeat for LSH with different random parameters and Take mean: 
 	#	By increasing r you get more accurate estimate
-    r = 4
+    r = 24
     I_vec, Grad_vec = np.zeros(r), np.zeros((r,N,d))
 
     # Use multiprocess
@@ -450,6 +450,7 @@ def EDGE_top(X,Y,U=20):
         Grad_vec[i,:,:] = result[i][1]
 
     I = np.mean(I_vec)
+    I = -1 * I
     Grad_mat= np.mean(Grad_vec,0)
     return (I,Grad_mat)
 

solver.py
@@ -101,8 +101,8 @@ class Solver(object):
                 z_sample = torch.normal(mu, std)
 
                 # Normalize the sample matrix
-                y = y.float() / y.max().float()
-                logit = logit.float() / logit.max().float()
+                y_norm = y.float() / y.max().float()
+                logit_norm = logit.float() / logit.max().float()
 
                 #x_sample = x_sample.float() / x_sample.max().float()
                 #z_sample = z_sample.float() / z_sample.max().float()
@@ -116,12 +116,12 @@ class Solver(object):
                 #info_edge = EDGE((info_size_0, info_size_1))
                 #class_edge = EDGE((class_size_0, class_size_1))
                 info_loss = EDGE.apply(z_sample, x_sample)
-                class_loss = EDGE.apply(logit, y)
+                class_loss = EDGE.apply(logit_norm, y_norm)
                 #info_loss = -0.5*(1+2*std.log()-mu.pow(2)-std.pow(2)).sum(1).mean().div(math.log(2))
                 total_loss = class_loss + self.beta*info_loss
 
-                izy_bound = math.log(10,2) - class_loss
-                izx_bound = info_loss
+                izy_bound = - class_loss
+                izx_bound = - info_loss
 
                 self.optim.zero_grad()
                 total_loss.cpu().backward()